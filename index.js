e38d134f2a1d9bb3db44d7eb226712ab7a13f0ee
ElijahOluwasegun <elijahoseni15@gmail.com>
2025-04-15 09:09:12 +0300

    Updated JavaScript file to add a function that returns "rock", "paper", or "scissors" based on a random number between 0 and 2. Mapping 0 to "rock", 1 to "paper", 2 to "scissors".

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random()*3)
+    console.log(randomNumber)
+    if (randomNumber === 0 ) {
+        console.log("rock")
+    } else if (randomNumber === 1){
+        console.log("paper")
+    } else {
+        console.log("scissors")
+    }
+}
+
+getComputerChoice()
