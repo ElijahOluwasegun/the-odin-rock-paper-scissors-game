a7f27c04abf8b3b5db0933adb5f876ccd6b8a7fe
ElijahOluwasegun <elijahoseni15@gmail.com>
2025-05-03 18:37:16 +0300

    Updated script tag with code for step 6 of the Odin Project - Wrote the logic "that takes the human and computer player choices as arguments, plays a single round, increments the round winner_s score and logs a winner announcement" - Commented out earlier console functions that would cause the function to call itself multiple times.

--- index.js
+++ index.js
@@ -12,14 +12,68 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+/* 
+    console.log(getComputerChoice()) 
+    
+    Used to test whether the function returns the expected values
+
+*/
 
 function getHumanChoice() {
     let userChoice = prompt("rock, paper, scissors? ").toLowerCase()
     return userChoice
 }
 
-console.log(getHumanChoice())
+/* 
+    console.log(getHumanChoice()) 
+
+    Used to test whether the function returns the expected values
+
+*/
 
 let humanScore = 0
 let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+
+    if ((humanChoice === "rock" && computerChoice === "scissors")) {
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    }
+
+    else if ((humanChoice === "scissors" && computerChoice === "rock")) {
+        console.log("Computer wins! Rock beats Scissors");
+        computerScore++;
+    }
+
+    else if ((humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log("You win! Scissors beat paper");
+        humanScore++;
+    }
+    
+    else if ((humanChoice === "paper" && computerChoice === "scissors")) {
+        console.log("Computer wins! Scissors beat paper");
+        computerScore++;
+    }
+
+    else if ((humanChoice === "paper" && computerChoice === "rock")) {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    }
+
+    else if((humanChoice === "rock" && computerChoice === "paper")) {
+        console.log("Computer wins! Paper beats Rock");
+        computerScore++;
+    }
+    
+    else {
+        console.log("It's a Tie!");
+    }
+}
+
+const humanPick = getHumanChoice()
+console.log(humanPick)
+const computerPick = getComputerChoice()
+console.log(computerPick)
+
+playRound(humanPick, computerPick)
